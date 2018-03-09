a17f00709b8317a8563f8e22a0eeb27768ebc7ca
tanglinbing <tanglinbing@4399inc.com>
2018-03-09 16:44:37 +0800

    import assimp headers and lib

---
+++ leanopengl/3.1_assimp.cpp
@@ -0,0 +1,30 @@
+//
+// Created by jmu on 18-3-5.
+//
+#include <mylib/common.h>
+#include <mylib/Shader.h>
+#include <mylib/GlutWrapper.h>
+
+//glm
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#define LOG_TAG __LINE__
+
+static void onPreDraw(){
+
+}
+
+static void onDraw(){
+
+}
+
+int main(int argc, char* argv[]){
+    GlutWrapper(onPreDraw, onDraw)
+            .init(&argc, argv);
+    return 0;
+}
